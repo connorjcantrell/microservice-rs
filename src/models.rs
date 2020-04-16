9d63083fc582d90a3f079efbbc0a6f94e55fb408
connorjcantrell <connorcantrell@outlook.com>
2020-04-16 15:16:42 -0700

    Added documentation to main.rs

--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,4 @@
+// Besides the schema, Diesel also requires us to write a model. This we have to write ourselves, in src/models.rs
 use schema::messages;
 
 #[derive(Queryable, Serialize, Debug)]
@@ -8,6 +9,8 @@ pub struct Message {
     pub timestamp: i64,
 }
 
+// Diesel can directly associate the fields of our struct with the columns in the database
+// The table must be called "messages", as indicated by the table_name attribute.
 #[derive(Insertable, Debug)]
 #[table_name = "messages"]
 pub struct NewMessage {
